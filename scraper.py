efbea15be0d560adb4d6b1a842c0698d831b015f
ckcole <cole.ckc@gmail.com>
2020-02-03 15:18:21 -0800

    Ckc parse html (#3)
    * time, openings, date
    
    * updated parser to dropin, all fields parsed
    
    * scraper poops output to output.json

--- scraper.py
+++ scraper.py
@@ -10,14 +10,16 @@
 
 from lxml import html
 from datetime import datetime
+import json
 
 
-with open('test_reg.html', 'r') as f:
+with open('test_reg_dropin.html', 'r') as f:
     txt = f.read()
 tree = html.fromstring(txt)
 
 # Spot to start iterating.
 # children are 5 rows (weeks) of <tr>
+clinics = []
 base_table = tree.find_class('rsContentTable')[0]
 for week in base_table.getchildren():
     # week (obviously) has 7 (days) of <td>
@@ -29,14 +31,48 @@ for week in base_table.getchildren():
         # strip/parse date string and cast datetime > date
         parsed_date = datetime.fromisoformat(
             date_header['href'].replace('#', '')
-        ).date
+        ).date()
+        for clinic in day.find_class("rsApt rsAptColor"):
+            # initialize the clinic's dictionary with the date field
+            clinic_dict = {'date': parsed_date.isoformat()}
+            # the data is html in string form in the "title" of attributes
+            nested_tree = html.fromstring(clinic.attrib['title'])
+            # nested_tree children index, attrib, text
+            # 0 {'class': 'wrToolTipHeader'} Gretzky Hour
+            # 1 {'class': 'wrToolTipTime'} 1/3/20  1:45 PM - 3:15 PM
+            # 2 {'class': 'wrToolTipHeaderBorder clear'} None
+            # 3 {'class': 'wrToolTipLabel clear'} Description:
+            # 4 {'class': 'wrToolTipValue clear'} Stick time for players
+            # 5 {'class': 'wrToolTipLabel clear'} Fees:
+            # 6 {'class': 'wrToolTipValue clear'} Gretzky Hour - 1.5 Hour $16.00
+            # 7 {} None
+            # 8 {'class': 'wrToolTipLabel clear'} Available Openings: 16
+            clinic_data = nested_tree.getchildren()
+            start, end = clinic_data[1].text.split(' - ')
+            clinic_dict['start'] = datetime.strptime(start, "%m/%d/%y %I:%M %p").time().isoformat()
+            clinic_dict['end'] = datetime.strptime(end, "%I:%M %p").time().isoformat()
+            clinic_dict['available_openings'] = int(clinic_data[8].text.split(': ')[1])
+            clinic_dict['meta'] = clinic_data[6].text
+            clinic_dict['tags'] = [x.attrib['alt'] for x in clinic.findall('*//img')]
+            clinics.append(clinic_dict)
 
-        # holy shit the text is nested 9 tags deep and theres nested html
-        # day.getchildren()[1].getchildren()[0].attrib['title'] has the html
-        # if the tag with the time range has an image,  that is one of the key words
-        # that image is not in the nested.
-        # day.getchildren()[1].getchildren()[0]
-        
+#             if parsed_date == datetime(2020, 1, 3).date():
+#                 raise(Exception('breaking to look at data'))
+
+with open('output.json', 'w') as f:
+    f.write(json.dumps(clinics))
+
+
+
+# holy shit the text is nested 9 tags deep and theres nested html
+# day.getchildren()[1].getchildren()[0].attrib['title'] has the html
+# if the tag with the time range has an image,  that is one of the key words
+# that image is not in the nested.
+
+
+# to extract the first part of the time:
+# datetime.strptime('1/3/20 1:45 PM', "%m/%d/%y %I:%M %p")
+
 
 
 # tree.find_class('rsContent rsMonthView')
